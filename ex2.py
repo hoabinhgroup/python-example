5645da4270e010dd214718fe2de534d170fb67af
hoabinhgroup <louis.standbyme@gmail.com>
2025-07-04 11:51:49 +0700

    Add initial implementation of list, tuple, dictionary, and set examples

---
+++ ex2.py
@@ -0,0 +1,34 @@
+#Hiểu và sử dụng List, Tuple, Dictionary, Set.
+# fruits = ["apple", "banana", "cherry"]
+
+# fruits.append("orange")  # Thêm phần tử vào danh sách
+# print(fruits)  # In danh sách sau khi thêm
+
+# fruits.remove("banana")  # Xóa phần tử khỏi danh sách
+# print(fruits)  # In danh sách sau khi xóa
+
+# for fruit in fruits:  # Duyệt qua từng phần tử trong danh sách
+#     print(fruit)  # In từng phần tử 
+
+ #TUPLE – BỘ Giống list nhưng không thay đổi được, dùng để lưu hằng số:
+# person = ("Nam", 22, 1.7, True)
+# print(person)  # In tuple
+
+#DICT – TỪ ĐIỂN
+
+# student = {
+#     "name": "Nam",
+#     "age": 22,
+#     "height": 1.7,
+#     "is_student": True
+# }
+
+# print(student["name"])  
+# for key,value in student.items():
+#  print(f"{key}: {value}") # In dictionary
+
+# hàm
+def greet(name):
+    print(f"Hello, {name}!")
+
+greet("Tuấn")
