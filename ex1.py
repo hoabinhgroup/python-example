c9b89462963bc9805425e8bc2fe967ee128d7edb
hoabinhgroup <louis.standbyme@gmail.com>
2025-07-04 11:04:36 +0700

    first commit

---
+++ ex1.py
@@ -0,0 +1,23 @@
+#print("Hello, world!")
+
+# name = input("Enter your name: ")
+# print(f"Hello, {name}!")
+# name = "Nam"
+# age = 22
+# height = 1.7
+# is_student = True
+
+# print(name, type(name))
+# print(age, type(age))
+# print(height, type(height))
+# print(is_student, type(is_student))
+
+# name = input("Nhập tên của bạn: ")
+# age = input("Nhập tuổi của bạn: ")
+
+# #print("Xin chào", name + ", bạn", age + " tuổi.")
+# print(f"Xin chào {name}, bạn {age} tuổi.")
+
+#Viết chương trình in bảng cửu chương 8
+for i in range(1, 11):
+    print(f"8 x {i} = {8 * i}")
